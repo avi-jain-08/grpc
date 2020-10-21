e75df9aecd421cb323124ea8e569a2b7597e9cc6
Avi <evanth.jain@honeywell.com>
2020-10-21 19:52:36 +0530

    First commit

---
+++ GrpcService/Services/TestService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServer;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class TestService : SampleService.SampleServiceBase
+    {
+        readonly ILogger _logger;
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+        public async override Task<ResponseDetails> LoadDetails(RequestDetails request, ServerCallContext context)
+        {
+            return await Task.FromResult(new ResponseDetails()
+            {
+                Address = "tester,usa",
+                Email = "tester@gmail.com",
+                EmployeeName = "test",
+                Phone = 12345,
+                Status = true
+            });
+
+            //return base.LoadDetails(request, context);
+        }
+        public override Task LoadEmployeeDetails(RequestEmployees request, IServerStreamWriter<ResponseDetails> responseStream, ServerCallContext context)
+        {
+            return base.LoadEmployeeDetails(request, responseStream, context);
+        }
+    }
+}
---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace gRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var serverAddress = "http://localhost:8099";//docker hosted url
+          //  var serverAddress = "http://localhost:32812";//docker hosted url
+
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            //httpHandler.ServerCertificateCustomValidationCallback =
+            //    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using var channel = GrpcChannel.ForAddress(serverAddress);
+            //using var channel = GrpcChannel.ForAddress(serverAddress,new GrpcChannelOptions { HttpHandler= httpHandler });
+            var client = new SampleService.SampleServiceClient(channel);
+            // unary
+            var reply = await client.LoadDetailsAsync(
+                new RequestDetails()
+                {
+                    Address = "",
+                    Email = "",
+                    EmployeeName = "",
+                    Phone = 0,
+                    Status = false
+                });
+            Console.WriteLine("Name: " + reply.EmployeeName);
+            Console.WriteLine("Address: " + reply.Address);
+            Console.WriteLine("Phone: " + reply.Phone);
+            Console.WriteLine("Email: " + reply.Email);
+            Console.WriteLine("Status: " + reply.Status);
+            Console.WriteLine();
+            Console.WriteLine("Stream object");
+            Console.WriteLine();
+        }
+    }
+}
